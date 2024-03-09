2f18aa950639e6fbfcd497ca6a20c33af1426c20
Allan Gong <hanyun.gong@gmail.com>
2024-03-09 12:59:49 -0800

    adjust spacing on site title section

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-5 mb-5 md:mb-5">
       <section>
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         PARS
       </h1>
-      <h4 className="text-center md:text-left text-lg">
+      <h4 className="md:text-4xl text-center md:text-left text-lg font-bold tracking-tighter leading-tight">
         Plumbing services in WA
       </h4>
       
