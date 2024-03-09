16f71a872f8a905a7165ecdf3c4eb14d058d85d9
Allan Gong <hanyun.gong@gmail.com>
2024-03-09 12:40:27 -0800

    initial setup

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <section className="mb-10">
+    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-1 mt-4">
       <Link href="/" className="hover:underline">
-        Blog
+        PARS Plumbing 
       </Link>
-      .
     </h2>
+    <p>IMAN: <Link href="tel:206-899-7743">206-899-7743</Link></p>
+    <p>AMIN: <Link href="tel:425-469-5033">425-469-5033</Link></p>
+    </section>
   );
 };
 
--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,21 +1,22 @@
 import { CMS_NAME } from "@/lib/constants";
+import Link from "next/link";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <section>
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        PARS
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+      <h4 className="text-center md:text-left text-lg">
+        Plumbing services in WA
       </h4>
+      
+    </section>
+    <section>
+      <p className="text-4xl">IMAN: <Link href="tel:206-899-7743">206-899-7743</Link></p>
+      <p className="text-4xl">AMIN: <Link href="tel:425-469-5033">425-469-5033</Link></p>
+    </section>
     </section>
   );
 }
