a06a063597a7e55e601db7e5c53f2e4dd093d4bc
Allan Gong <hanyun.gong@gmail.com>
2024-03-09 16:34:24 -0800

    adjust CSS for header

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-5 mb-5 md:mb-5">
+    <section className="flex-col md:flex-row flex md:justify-between mt-2 mb-2 md:mb-2">
       <section>
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         PARS
       </h1>
-      <h4 className="md:text-4xl text-center md:text-left text-lg font-bold tracking-tighter leading-tight">
+      <h4 className="md:text-4xl md:text-left text-lg font-bold tracking-tighter leading-tight">
         Plumbing services in WA
       </h4>
       
     </section>
-    <section>
-      <p className="text-4xl">IMAN: <Link href="tel:206-899-7743">206-899-7743</Link></p>
-      <p className="text-4xl">AMIN: <Link href="tel:425-469-5033">425-469-5033</Link></p>
+    <section className="mt-5 tracking-tighter leading-tight">
+      <p className="lg:text-2xl sm:text-sm	 md:text-sm	">IMAN: <Link href="tel:206-899-7743">206-899-7743</Link></p>
+      <p className="lg:text-2xl sm:text-sm	 md:text-sm	">AMIN: <Link href="tel:425-469-5033">425-469-5033</Link></p>
     </section>
     </section>
   );
